ff4ac602a9c3ce0f12c93a88a9e6c3900a315822
PalashHalder <halder.palash08@gmail.com>
2023-01-05 14:23:11 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+console.log('Hello, Bangladesh');
+
+let x = 5;
+console.log(x);
+
+x = 3;
+console.log(x);
+
+let sum = 44;
+console.log(sum);   // 44
+
+
+
+
+const greet = 'Hello';
+const person = 'Jack';
+
+console.log(greet +' '+ person);
+
+
+
+//strings example
+const person1 = 'ram';
+const person2 = "hari";
+const result = `The names are ${person1} and ${person2}`;
+
+console.log(result);
+
+
+
+// BigInt value
+const value1 = 900719925124740998n;
+
+// Adding two big integers
+const result1 = value1 + 1n;
+console.log(result1); // "900719925124740999n"
+
+const value2 = 900719925124740998n;
+
+// Error! BitInt and number cannot be added
+const result2 = value2 + 1; 
+console.log(result2); 
+
+/*
+জাভাস্ক্রিপে if ... else এর তিনটি ফর্ম রয়েছে:
+    1. if statement
+    2. if...else Statement
+    3. if ... else .. if...else statement
+
+    The syntax of if statement:
+    if(condition){
+        //The body of if
+    }
+    যদি শর্ত সত্য (true) হয় তাহলে বডি (body) কাজ করে;
+    যদি শর্ত মিথ্যা (false) হয় তাহলে বডি (body) skip করে;
+   
+  
+    // Example-01
+    // Condition is true
+
+    let number = 2;
+    if (number > 0) {
+        //code
+    }
+    //code after if
+
+    // Example-01
+    // Condition is true
+
+    let number1 = -2;
+    if (number1 > 0) {
+        // code
+    }
+    //code after if
+*/
